1dc7da66aadf0c34f0e3f46f50a6dc50e2bf6239
syoung125 <laura120555@gmail.com>
2020-07-14 15:22:01 +0900

    until #2.7

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000
 
-function handleListening(){
-    console.log('Listening on: http://localhoast:4000');
-}
-function handleHome(req, res){
-    res.send('Hi from home!!');
-}
+// arrow function
+const handleListening = () => console.log('Listening on: http://localhoast:4000');
+
+const handleHome = (req, res) => res.send("Hi from home");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend:true }));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.get('/', handleHome);
+
 
 app.listen(PORT, handleListening);
