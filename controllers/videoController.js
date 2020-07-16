71c1467b1d583d4df95912bbd8df4ef0b00e187b
syoung125 <laura120555@gmail.com>
2020-07-16 13:10:22 +0900

    Until #2.16 Pug layout, partial, locals

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("VIDEO_DETAIL");
-export const editVideo = (req, res) => res.send("EDIT_VIDEO");
-export const deleteVideo = (req, res) => res.send("DELETE_VIDEO");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
+export const videos = (req, res) => res.render("videos");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
