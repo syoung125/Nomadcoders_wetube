2e17585456b2a8ca622c3334622d32a85c1cc8dd
syoung125 <laura120555@gmail.com>
2020-07-17 11:52:23 +0900

    #2.21 Home Controller

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # WeTube
 
 Cloning YouTube with Vanilla and Node JS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
+
---
+++ views/db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 323413,
+        title: "Video awesome",
+        description: "this is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Seoyoung",
+            email: "sysy@gmail.com"
+        }
+    },
+    {
+        id: 334563,
+        title: "Boogie On&On",
+        description: "Handsomeee",
+        views: 30,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 232343,
+            name: "Sooyoung",
+            email: "ksy@gmail.com"
+        }
+    },
+    {
+        id: 567856,
+        title: "Hello",
+        description: "Hola Que tal?",
+        views: 4,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Seoyoung",
+            email: "sysy@gmail.com"
+        }
+    },
+    {
+        id: 123123,
+        title: "abcdefg",
+        description: "hijklmnopqrstuvwxyz",
+        views: 23,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "abc",
+            email: "abc@gmail.com"
+        }
+    }
+];
